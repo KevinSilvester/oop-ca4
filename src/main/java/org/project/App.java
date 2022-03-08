2014cdb265938e3dfc4e2999499f060329700b39
Kevin Silvester <kevinsilvester05@gmail.com>
2022-03-08 13:32:34 +0000

    Features 3 and 4 complete

--- src/main/java/org/project/App.java
+++ src/main/java/org/project/App.java
@@ -1,14 +1,11 @@
 package org.project;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
     private static final Scanner KB = new Scanner(System.in);
     private static List<Movie> movieList;
+    private static HashMap<Integer, Movie> movieHashMap;
 
     public static void main( String[] args ) {
         App app = new App();
@@ -19,11 +16,13 @@ public class App {
         System.out.println("Hello World");
         System.out.println("Project part 1 - CA5");
         movieList = new ArrayList<>();
-        initialize(movieList);
+        movieHashMap = new HashMap<>();
+        populateArrayList(movieList);
+        populateHashMap(movieList, movieHashMap);
         menu();
     }
 
-    private void initialize(List<Movie> list) {
+    private void populateArrayList(List<Movie> list) {
        list.add(new Movie(
                "The fast and the Furious",
                2001,
@@ -137,16 +136,29 @@ public class App {
         ));
     }
 
+    public void populateHashMap(List<Movie> list, HashMap <Integer, Movie> map) {
+        int index = 0;
+
+        for (Movie m : list) {
+            map.put(++index ,m);
+        }
+//        for (int i = 0; i < list.size(); i++) {
+//      System.out.println(i);
+//            map.put(i ,list.get(i));
+//        }
+    }
+
     private void menu() {
         final String MENU = "\n*** MENU ***\n"
                 + "1. Display all Movies\n"
-                + "2. Exit\n"
+                + "2. Find Movie by Key\n"
+                + "3. Exit\n"
                 + "Enter Option [1, 2]";
 
-        final int DISPLAY_APP   = 1;
-        final int EXIT          = 2;
+        final int DISPLAY_LIST  = 1;
+        final int FIND_IN_MAP   = 2;
+        final int EXIT          = 3;
 
-        boolean valid = true;
         int option = 0;
 
         do {
@@ -155,9 +167,14 @@ public class App {
                 String usersInput = KB.nextLine();
                 option = Integer.parseInt(usersInput);
                 switch (option) {
-                    case DISPLAY_APP:
+                    case DISPLAY_LIST:
                         System.out.println("Display ALL Movies");
                         displayList(movieList);
+                        break;
+
+                    case FIND_IN_MAP:
+                        System.out.println("Find Movie by Key");
+                        findByKey();
                         break;
 
                     case EXIT:
@@ -168,7 +185,6 @@ public class App {
                         System.out.print("Invalid option - please enter number in range");
                         break;
                 }
-
             } catch (InputMismatchException | NumberFormatException e) {
                 System.out.print("Invalid option - please enter number in range");
             }
@@ -182,6 +198,7 @@ public class App {
         int boxOfficeTab = -12;
         int directorTab = -16;
         int actorsTab = -18;
+
         System.out.println(
             "\n============================================\n"
             + "\t\t\tMovie ArrayList\n"
@@ -194,6 +211,7 @@ public class App {
                 "%" + directorTab + "s" +
                 "%" + actorsTab + "s%n",
                 "Title", "Year", "BoxOffice", "Director", "Actors");
+
         for (Movie m: list) {
             sb.append(String.format("%" + titleTab + "s", m.getTitle()));
             sb.append(String.format("%" + yearTab + "d", m.getYear()));
@@ -201,8 +219,55 @@ public class App {
             sb.append(String.format("%" + directorTab + "s", m.getDirector()));
             sb.append(String.format("%" + actorsTab + "s", m.getActors()));
             sb.append(String.format("%n"));
-            //sb.append(String.format("%" + titleTab + "s%-7d%-7.1f%-7s%n", m.getTitle(), m.getYear(), m.getBoxOffice(), m.getDirector()));
         }
         System.out.println(sb);
+    }
+
+    public void findByKey() {
+        List<Movie> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        String keyStr = null;
+        int titleTab = -40;
+        int yearTab = -7;
+        int boxOfficeTab = -12;
+        int directorTab = -16;
+        int actorsTab = -18;
+        int key = 0;
+
+        System.out.println(
+                "\n============================================\n"
+                + "\t\t\tFind by Key\n"
+                + "============================================");
+
+        try {
+            System.out.print("Enter a key: ");
+            keyStr = KB.nextLine();
+            key = Integer.parseInt(keyStr);
+
+            Movie movie = movieHashMap.get(key);
+            if (movie != null) {
+                System.out.println("*** Movie Found ***");
+                System.out.printf(
+                        "%" + titleTab + "s" +
+                        "%" + yearTab + "s" +
+                        "%" + boxOfficeTab + "s" +
+                        "%" + directorTab + "s" +
+                        "%" + actorsTab + "s%n",
+                        "Title", "Year", "BoxOffice", "Director", "Actors");
+
+                sb.append(String.format("%" + titleTab + "s", movie.getTitle()));
+                sb.append(String.format("%" + yearTab + "d", movie.getYear()));
+                sb.append(String.format("%" + boxOfficeTab + ".1f", movie.getBoxOffice()));
+                sb.append(String.format("%" + directorTab + "s", movie.getDirector()));
+                sb.append(String.format("%" + actorsTab + "s", movie.getActors()));
+                sb.append(String.format("%n"));
+                System.out.println(sb);
+            } else {
+                System.out.println("Key does not match any Movie");
+            }
+
+        } catch (InputMismatchException | NumberFormatException e) {
+            System.out.print("Invalid Input - Key has to a whole number");
+        }
     }
 }
